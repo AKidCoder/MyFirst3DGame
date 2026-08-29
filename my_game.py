7be6e68ee3eccfe9c2209f24596fc5f259dd6cd0
AKidCoder <forgithub1120@gmail.com>
2026-08-29 15:07:52 +0800

    add coin system

--- my_game.py
+++ my_game.py
@@ -7,6 +7,8 @@ EYE_HEIGHT = 2.0 #Your Height
 MAP_SIZE  = 14 #stadiums's half radious
 BOX_SIZE  = 2.0#length of the side
 BOX_COUNT = 12  #how many boxes
+TOTAL_COINS = 10 #how many coins should I eat and generate
+PICK_RANGE = 1.5 #The range that the system will let me eat the coins
 
 init_window(900, 600, "First_3D")
 set_target_fps(60)
@@ -20,6 +22,16 @@ while len(boxes) < BOX_COUNT:
     if math.hypot(x, z) > 4: #don't block the spawn
         boxes.append((x, z))
 
+coins = []
+while len(coins) < TOTAL_COINS:
+    x = random.uniform(-MAP_SIZE, MAP_SIZE)
+    z = random.uniform(-MAP_SIZE, MAP_SIZE)
+    if math.hypot(x, z) > 3:
+        coins.append((x, z))  
+
+score = 0
+elapsed = 0.0 
+
 camera = Camera3D(
     Vector3(0, EYE_HEIGHT, 0), #where are you
     Vector3(0, EYE_HEIGHT, -1), #where are you looking
@@ -27,8 +39,20 @@ camera = Camera3D(
     60.0, #FOV(how far can you see)
     CAMERA_PERSPECTIVE
 )
+
+
 #The main code is the heart of the whole thing
 while not window_should_close():
+    dt = get_frame_time()
+    elapsed += dt
+    for coin in list(coins) : #don't forget the list(), very important
+        cx, cz = coin
+        dist = math.hypot(camera.position.x - cx,
+                        camera.position.z - cz)
+        if dist < PICK_RANGE:
+            coins.remove(coin)
+            score += 1
+        
     update_camera(camera, CAMERA_FIRST_PERSON)
     begin_drawing()
     clear_background(SKYBLUE)
@@ -38,12 +62,15 @@ while not window_should_close():
     draw_grid(MAP_SIZE*2 +10, 1)
     for bx, bz in boxes:
         draw_cube(Vector3(bx, BOX_SIZE/2, bz),
-                  BOX_SIZE, BOX_SIZE, BOX_SIZE, GRAY)
-        
-        
+            BOX_SIZE, BOX_SIZE, BOX_SIZE, GRAY)
+    for cx, cz in coins:
+        bob =  1.0 + math.sin(elapsed * 3 + cx) * 0.2
+        draw_sphere(Vector3(cx, bob, cz), 0.4 , GOLD)
+      
     end_mode_3d()
 
     draw_text("+", 444, 290, 20, WHITE)
+    draw_text(f"Coins: {score} / {TOTAL_COINS}", 20, 20 , 28, BLACK)
 
     end_drawing()
 
