7448a86ee9434fa4740b90c5b8646761cb75741a
AKidCoder <forgithub1120@gmail.com>
2026-09-06 11:31:31 +0800

    add hit_box

--- my_game.py
+++ my_game.py
@@ -1,15 +1,15 @@
 import math
 import random
 from pyray import *
-
+from time import *
 
 EYE_HEIGHT = 2.0 #Your Height
 MAP_SIZE  = 14 #stadiums's half radious
 BOX_SIZE  = 2.0#length of the side
-BOX_COUNT = 12  #how many boxes
+BOX_COUNT = 9  #how many boxes
 TOTAL_COINS = 10 #how many coins should I eat and generate
-PICK_RANGE = 1.5 #The range that the system will let me eat the coins
-
+PICK_RANGE = 1.5#The range that the system will let me eat the coins
+PLAYER_R = 0.4
 init_window(900, 600, "First_3D")
 set_target_fps(60)
 disable_cursor()
@@ -40,9 +40,18 @@ camera = Camera3D(
     CAMERA_PERSPECTIVE
 )
 
+#This is where I wrote the hitbox
 
+def hits_box(px, pz):
+    half = BOX_SIZE/2 + PLAYER_R
+    for bx, bz in boxes:
+        if abs(px - bx) < half and abs(pz - bz) < half:
+            return True
+    return False
 #The main code is the heart of the whole thing
 while not window_should_close():
+    old_x = camera.position.x #remember the old position of x and z
+    old_z = camera.position.z
     dt = get_frame_time()
     elapsed += dt
     for coin in list(coins) : #don't forget the list(), very important
@@ -54,6 +63,7 @@ while not window_should_close():
             score += 1
         
     update_camera(camera, CAMERA_FIRST_PERSON)
+    px, pz = camera.position.x, camera.position.z
     begin_drawing()
     clear_background(SKYBLUE)
     begin_mode_3d(camera)
@@ -66,12 +76,21 @@ while not window_should_close():
     for cx, cz in coins:
         bob =  1.0 + math.sin(elapsed * 3 + cx) * 0.2
         draw_sphere(Vector3(cx, bob, cz), 0.4 , GOLD)
+    if hits_box(px, pz) or abs(px) > MAP_SIZE + 2 or abs(pz) > MAP_SIZE + 2:
+        dx = old_x - camera.position.x
+        dz = old_z - camera.position.z
+        camera.position.x += dx
+        camera.position.z += dz
+        camera.target.x += dx        
+        camera.target.z += dz
       
     end_mode_3d()
 
     draw_text("+", 444, 290, 20, WHITE)
     draw_text(f"Coins: {score} / {TOTAL_COINS}", 20, 20 , 28, BLACK)
-
+    if score == TOTAL_COINS:
+        draw_text("YOU WIN", 340, 260, 50, MAROON)
+    draw_fps(20, 60)
     end_drawing()
 
 close_window()
